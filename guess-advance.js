50d30613b9639ce1ac7f110a0dcadfb908e0c5dd
Mlodestone <54862142+Mlodestone@users.noreply.github.com>
2019-11-22 14:46:22 -0500

    finished and checked

--- guess-advance.js
+++ guess-advance.js
@@ -2,45 +2,85 @@
  * Format: while (boolean expression) {} -- indent block code 
  * PEN BY MIMI LODESTONE
  */
-
-var answer = Math.floor(Math.random()*100)+1;
-console.log(answer);
-var guess = 0;
-var turns = 0;
-while (guess != answer) {
-	guess = prompt("guess the number 1-100");
-	if (guess == "q") {
+var game = 0;
+var again = true;
+var totalTurns = 0;
+while (again == true) {
+	var guess = 0;
+	var turns = 0;
+	game++;
+	var answer = Math.floor(Math.random()*100)+1;
+	console.log(answer);
+	while (guess != answer) {
+		guess = prompt("guess the number 1-100");
+		if (guess == "q") {
+			alert("Quitter!");
+			break;
+		}
+		if (validator(guess) == true) {
+			turns++;
+			if (guess < answer) {
+				alert("too low");
+			} else if (guess > answer) {
+				alert("too high");
+			}
+		} else if (validator(guess) == false){
+			alert("invalid guess!");
+		}
+	}
+	if (guess == answer) {
+		gameStats(turns);
+		again = newGame();
+	} else {
 		break;
 	}
-	if (validator(guess) == true) {
-		turns++;
-		if (guess < answer) {
-			alert("too low");
-		} else if (guess > answer) {
-			alert("too high");
+
+	/* Function validator
+	*Checks if guess is valid integer within range (1-100 default)
+	*If guess not valid return false;
+	* If guess valid, return true;
+	* @param guess
+	* @return boolean
+	*/
+
+	function validator(guess) {
+		if (guess > 0 && guess < 101) {
+			return true;
+		} else {
+			return false;
 		}
-	} else if (validator(guess) == false){
-		alert("invalid guess!");
 	}
-}
-if (guess == answer) {
-	alert("you got it in "+turns+" turns");
-} else {
-	alert("Quitter!");
-}
-
-/* Function validator
-*Checks if guess is valid integer within range (1-100 default)
-*If guess not valid return false;
-* If guess valid, return true
-* @param guess
-* @return boolean
-*/
-
-function validator(guess) {
-	if (guess > 0 && guess < 101) {
-		return true;
-	} else {
-		return false;
+	
+	
+	/* Function gameStats
+	* Tells player how many turns it took them;
+	* Divides the total turns of every game by the amount of games played to get the average turns;
+	* Tells player the amount of games played and the average turns;
+	* @param turns
+	*/
+	
+	function gameStats(turns) {
+		alert("you guessed it in "+turns+" turns!");
+		totalTurns = totalTurns + turns;
+		averageTurns = totalTurns/game;
+		alert("you won "+game+" games with an average of "+averageTurns+" turns per game!");
+	}
+	
+	
+	/* Function newGame
+	* Asks player if they want to play again;
+	* If false "thanks for playing" and return false;
+	* If true return true;
+	* @return boolean
+	*/
+	
+	function newGame() {
+		again = confirm("play again?");
+		if (again == false) {
+			alert("thanks for playing!");
+			return false;
+		} else if (again == true) {
+			return true;
+		}
 	}
 }
